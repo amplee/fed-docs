0ae981fd8fc4a4b7e3697bf41f5ad2fc5629dbb8
连展鹏 <zhanpeng.lian@wehotelglobal.com>
2019-04-25 18:09:00 +0800

    update: 初始化项目

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,10 +2,10 @@ module.exports = {
     port: 8302,
     dest: 'docs',
     evergreen: true,
+    title: 'WeHotel 前端文档',
     // permalink: '/:slug'
     themeConfig: {
         nav: [
-            {text: '首页', link: '/'},
             {text: '代码规范', link: '/style-guide/'},
             {text: '私有npm', link: '/verdaccio/'},
             {text: 'GitLab', link: 'http://172.25.38.44/fe-gayhub/fe-docs'}
